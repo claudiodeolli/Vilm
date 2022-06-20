19ca979af1823849ad95720b69f8243c7c45442b
Arcaino <claudio.dzuwa@gmail.com>
2022-06-20 14:02:30 -0300

    chore: node server added

---
+++ client/js/app/app.js
@@ -0,0 +1,24 @@
+import MainPage from "./pages/main-page/main-page.js";
+
+const app = document.querySelector('router-outlet');
+const routes = {
+    '/': '<main-page></main-page>',
+    //transferir para routes
+    '/details': '<details-page></details-page>'
+}
+
+app.innerHTML = routes[window.location.pathname];
+
+export const onNavigate = (pathName) => {
+    window.history.pushState(
+        {}, 
+        pathName, 
+        window.location.origin + pathName
+    );
+    console.log(routes);
+    app.innerHTML = routes[pathName];
+};
+
+window.onpopstate = () => {
+    app.innerHTML = routes[window.location.pathname];
+};
